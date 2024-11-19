001e8ec8322fe4df38e1187871e34977c2b1fbb9
nvatlin <nvatlin91@gmail.com>
2024-11-19 15:22:31 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide(first, second):
+    sec = int(second)
+    fir = int(first)
+    if second == 0:
+        return "Ошибка"
+    else:
+        return fir / sec
+
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+
+def divide(first, second):
+    sec = int(second)
+    fir = int(first)
+    if second == 0:
+        return inf
+    else: return fir / sec
